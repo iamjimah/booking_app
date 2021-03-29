1f371022f84d46ec3171b45c44785a092065bb03
iamjimah <iamjimah@gmail.com>
2021-03-29 17:26:17 +0000

    committed

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div>
+      <h1>CRAWFORD INSTITUTE OF TECHNOLOGY</h1>
+      <h4> EXEAT BOOKING SYSTEM FOR STUDENTS</h4>
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/components/Students.js
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { StudentContext } from "../context/StudentContext";
+
+function Students() {
+  const { students, dispatch } = useContext(StudentContext);
+
+  return (
+    <div className="card-container">
+      {students.map((student) => (
+        <div className="card page">
+          <h2>{student.name}</h2>
+          <h2>{student.level}</h2>
+          <h2>{student.date}</h2>
+          <button
+            className="btn btn-danger"
+            onClick={() =>
+              dispatch({ type: "delete", indexNo: student.indexNo })
+            }
+          >
+            X
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Students;
